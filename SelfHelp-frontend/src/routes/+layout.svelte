62823ca4237a161e2f5145581ebef427ede84e22
Sameer <sameer.barha12@gmail.com>
2025-08-25 00:55:21 +0530

    added landing page

--- SelfHelp-frontend/src/routes/+layout.svelte
+++ SelfHelp-frontend/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	
+	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -9,3 +11,17 @@
 </svelte:head>
 
 {@render children?.()}
+
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		/*padding: 1rem;*/
+		width: 100%;
+		max-width: 100%;
+		margin: 0 auto;
+		box-sizing: border-box;
+		min-height: 100vh;
+	}
+</style>
